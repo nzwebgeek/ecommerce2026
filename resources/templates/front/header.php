e5d31f85bb88e78fe3625810937fd1b6630cd3fd
mikenzwebgeek <mikenzwebgeek@gmail.com>
2026-02-10 15:37:37 +1300

    `reduced space below nav bar

---
+++ resources/templates/front/header.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Marthas Cuisine - Online Ordering 2026</title>
+
+    <!-- Bootstrap 4.6.2 CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
+
+    <!-- Custom CSS -->
+    <link href="css/shop-homepage.css" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+
+</head>
+
+<body>
+
+    <!-- Navigation -->
+    <!--<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">-->
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top navbar-skinny" role="navigation">
+  <!-- Navbar content goes here -->
+           <?php include(TEMPLATE_FRONT . DS . "top_nav.php"); ?>
+</nav>
+
+
+        <!-- /.container -->
+   <!-- </nav>-->
